0ade9e5c97aa9c1a5bb192c87609070468dab8eb
JEETKADAM1309 <jeetkadam2008@gmail.com>
2026-09-07 13:30:57 +0530

    Add files via upload

---
+++ librarysort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int book[5];
+   cout << "enter 5 book IDs:\n";
+   for (int i = 0; i < 5; i++)
+   {
+       cin >> book[i];
+   }
+   
+   for (int i = 0; i < 4; i++)
+   {
+       for (int j = 0; j < 4; j++)
+       {
+           if (book[j] > book[j + 1])
+           {
+              int temp = book[j];
+              
+              book[j] = book[j+1];
+              book[j+1] = temp;
+           }
+       }
+   }
+cout << "\nbook after sorting:\n";
+for (int i = 0; i<5; i++)
+{
+   cout << book[i] << " ";
+}
+return 0;
+}  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+          
