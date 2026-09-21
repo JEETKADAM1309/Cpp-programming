fd2b7a80aade3c3113bc8fa50f0dbbb903e33658
JEETKADAM1309 <jeetkadam2008@gmail.com>
2026-09-21 11:15:03 +0530

    Add files via upload

---
+++ student_display.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rollNo[5];
+
+    cout << "Enter roll numbers of 5 students:\n";
+    for (int i = 0; i < 5; i++) {
+        cin >> rollNo[i];
+    }
+
+    cout << "\nRoll numbers entered are:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << rollNo[i] << endl;
+    }
+
+    return 0;
+}
+
